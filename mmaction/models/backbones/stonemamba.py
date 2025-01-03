27da2591aaf10c33ac7853a69cb0f9b50bea97fe
Sangyun Lee <sylee@skhu.ac.kr>
2025-01-03 19:51:00 +0900

    official mamba

--- mmaction/models/backbones/stonemamba.py
+++ mmaction/models/backbones/stonemamba.py
@@ -15,31 +15,7 @@ from mmcv.cnn import ConvModule
 from ..utils import Graph, unit_gcn, mstcn, unit_tcn
 from timm.models.layers import Mlp, DropPath, trunc_normal_
 
-def import_class(name):
-    components = name.split('.')
-    mod = __import__(components[0])
-    for comp in components[1:]:
-        mod = getattr(mod, comp)
-    return mod
-
-@dataclass
-class ModelArgs:
-    d_model: int
-    d_state: int = 16
-    expand: int = 2
-    dt_rank: Union[int, str] = 'auto'
-    d_conv: int = 3
-    conv_bias: bool = True
-    bias: bool = False
-    
-    
-    def __post_init__(self):
-        self.d_inner = int(self.expand * self.d_model)
-        
-        if self.dt_rank == 'auto':
-            self.dt_rank = math.ceil(self.d_model / 16)
-
-            
+from mamba_ssm import Mamba, Mamba2
 
 @MODELS.register_module()
 class StoneMamba(BaseModule):
@@ -141,8 +117,8 @@ class ResidualBlock(nn.Module):
         self.tconv = nn.Conv2d(in_channels // 2, in_channels // 2, kernel_size=(9, 1),
                                padding=((9 - 1) // 2, 0), groups=8)
 
-        self.mixer = MambaBlock(ModelArgs(d_model=in_channels*A.size(-1)))
-        self.norm = RMSNorm(in_channels*A.size(-1))
+        self.mamba = Mamba(d_model=in_channels*A.size(-1), d_state=16, d_conv=4, expand=2)
+        self.norm = nn.LayerNorm(in_channels*A.size(-1))
 
         self.out_proj = nn.Linear(in_features=in_channels*2, out_features=in_channels, bias=True)
         self.out_norm = nn.LayerNorm(in_channels)
@@ -188,7 +164,7 @@ class ResidualBlock(nn.Module):
 
         # Mamba
         out_mamba = rearrange(x_mamba, 'n c t v -> n t (v c)')
-        out_mamba = self.mixer(self.norm(out_mamba))+out_mamba
+        out_mamba = self.norm(self.mamba(out_mamba)+out_mamba)
         out_mamba = rearrange(out_mamba, 'n t (v c) -> n c t v', v=V, c=C)
         out.append(out_mamba)
 
@@ -199,169 +175,3 @@ class ResidualBlock(nn.Module):
         output = output.permute(0, 3, 1, 2).contiguous()
         
         return output
-
-        
-class MambaBlock(nn.Module):
-    def __init__(self, args: ModelArgs):
-        """A single Mamba block, as described in Figure 3 in Section 3.4 in the Mamba paper [1]."""
-        super().__init__()
-        self.args = args
-
-        self.in_proj = nn.Linear(args.d_model, args.d_inner * 2, bias=args.bias)
-
-        self.conv1d = nn.Conv1d(
-            in_channels=args.d_inner,
-            out_channels=args.d_inner,
-            bias=args.conv_bias,
-            kernel_size=args.d_conv,
-            groups=args.d_inner,
-            padding=args.d_conv - 1,
-        )
-        # x_proj takes in `x` and outputs the input-specific Δ, B, C
-        self.x_proj = nn.Linear(args.d_inner, args.dt_rank + args.d_state * 2, bias=False)
-        
-        # dt_proj projects Δ from dt_rank to d_in
-        self.dt_proj = nn.Linear(args.dt_rank, args.d_inner, bias=True)
-
-        A = repeat(torch.arange(1, args.d_state + 1), 'n -> d n', d=args.d_inner)
-        self.A_log = nn.Parameter(torch.log(A))
-        self.A_log._no_weight_decay = True
-        self.D = nn.Parameter(torch.ones(args.d_inner))
-        self.D._no_weight_decay = True
-        self.out_proj = nn.Linear(args.d_inner, args.d_model, bias=args.bias)
-        
-
-    def forward(self, x):
-        """Mamba block forward. This looks the same as Figure 3 in Section 3.4 in the Mamba paper [1].
-    
-        Args:
-            x: shape (b, l, d)    (See Glossary at top for definitions of b, l, d_in, n...)
-    
-        Returns:
-            output: shape (b, l, d)
-        
-        Official Implementation:
-            class Mamba, https://github.com/state-spaces/mamba/blob/main/mamba_ssm/modules/mamba_simple.py#L119
-            mamba_inner_ref(), https://github.com/state-spaces/mamba/blob/main/mamba_ssm/ops/selective_scan_interface.py#L311
-            
-        """
-        (b, l, d) = x.shape
-        
-        x_and_res = self.in_proj(x)  # shape (b, l, 2 * d_in)
-        (x, res) = x_and_res.split(split_size=[self.args.d_inner, self.args.d_inner], dim=-1)
-
-        x = rearrange(x, 'b l d_in -> b d_in l')
-        x = self.conv1d(x)[:, :, :l]
-        x = rearrange(x, 'b d_in l -> b l d_in')
-        
-        x = F.silu(x)
-
-        y = self.ssm(x)
-        
-        y = y * F.silu(res)
-        
-        output = self.out_proj(y)
-
-        return output
-
-    
-    def ssm(self, x):
-        """Runs the SSM. See:
-            - Algorithm 2 in Section 3.2 in the Mamba paper [1]
-            - run_SSM(A, B, C, u) in The Annotated S4 [2]
-
-        Args:
-            x: shape (b, l, d_in)    (See Glossary at top for definitions of b, l, d_in, n...)
-    
-        Returns:
-            output: shape (b, l, d_in)
-
-        Official Implementation:
-            mamba_inner_ref(), https://github.com/state-spaces/mamba/blob/main/mamba_ssm/ops/selective_scan_interface.py#L311
-            
-        """
-        (d_in, n) = self.A_log.shape
-
-        # Compute ∆ A B C D, the state space parameters.
-        #     A, D are input independent (see Mamba paper [1] Section 3.5.2 "Interpretation of A" for why A isn't selective)
-        #     ∆, B, C are input-dependent (this is a key difference between Mamba and the linear time invariant S4,
-        #                                  and is why Mamba is called **selective** state spaces)
-        
-        A = -torch.exp(self.A_log.float())  # shape (d_in, n)
-        D = self.D.float()
-
-        x_dbl = self.x_proj(x)  # (b, l, dt_rank + 2*n)
-        
-        (delta, B, C) = x_dbl.split(split_size=[self.args.dt_rank, n, n], dim=-1)  # delta: (b, l, dt_rank). B, C: (b, l, n)
-        delta = F.softplus(self.dt_proj(delta))  # (b, l, d_in)
-        
-        y = self.selective_scan(x, delta, A, B, C, D)  # This is similar to run_SSM(A, B, C, u) in The Annotated S4 [2]
-        
-        return y
-
-    
-    def selective_scan(self, u, delta, A, B, C, D):
-        """Does selective scan algorithm. See:
-            - Section 2 State Space Models in the Mamba paper [1]
-            - Algorithm 2 in Section 3.2 in the Mamba paper [1]
-            - run_SSM(A, B, C, u) in The Annotated S4 [2]
-
-        This is the classic discrete state space formula:
-            x(t + 1) = Ax(t) + Bu(t)
-            y(t)     = Cx(t) + Du(t)
-        except B and C (and the step size delta, which is used for discretization) are dependent on the input x(t).
-    
-        Args:
-            u: shape (b, l, d_in)    (See Glossary at top for definitions of b, l, d_in, n...)
-            delta: shape (b, l, d_in)
-            A: shape (d_in, n)
-            B: shape (b, l, n)
-            C: shape (b, l, n)
-            D: shape (d_in,)
-    
-        Returns:
-            output: shape (b, l, d_in)
-    
-        Official Implementation:
-            selective_scan_ref(), https://github.com/state-spaces/mamba/blob/main/mamba_ssm/ops/selective_scan_interface.py#L86
-            Note: I refactored some parts out of `selective_scan_ref` out, so the functionality doesn't match exactly.
-            
-        """
-        (b, l, d_in) = u.shape
-        n = A.shape[1]
-        
-        # Discretize continuous parameters (A, B)
-        # - A is discretized using zero-order hold (ZOH) discretization (see Section 2 Equation 4 in the Mamba paper [1])
-        # - B is discretized using a simplified Euler discretization instead of ZOH. From a discussion with authors:
-        #   "A is the more important term and the performance doesn't change much with the simplification on B"
-        deltaA = torch.exp(einsum(delta, A, 'b l d_in, d_in n -> b l d_in n'))
-        deltaB_u = einsum(delta, B, u, 'b l d_in, b l n, b l d_in -> b l d_in n')
-        
-        # Perform selective scan (see scan_SSM() in The Annotated S4 [2])
-        # Note that the below is sequential, while the official implementation does a much faster parallel scan that
-        # is additionally hardware-aware (like FlashAttention).
-        x = torch.zeros((b, d_in, n), device=deltaA.device)
-        ys = []    
-        for i in range(l):
-            x = deltaA[:, i] * x + deltaB_u[:, i]
-            y = einsum(x, C[:, i, :], 'b d_in n, b n -> b d_in')
-            ys.append(y)
-        y = torch.stack(ys, dim=1)  # shape (b, l, d_in)
-        
-        y = y + u * D
-    
-        return y
-
-
-class RMSNorm(nn.Module):
-    def __init__(self,
-                 d_model: int,
-                 eps: float = 1e-5):
-        super().__init__()
-        self.eps = eps
-        self.weight = nn.Parameter(torch.ones(d_model))
-
-    def forward(self, x):
-        output = x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps) * self.weight
-        return output
-
